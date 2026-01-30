7c6cf25badb1ce8fdddc86252bbcb8e6a8251dbe
nelsonanto043 <nelsonanto043@gmail.com>
2026-01-30 14:43:25 +0530

    Problem 30

--- problem_30.py
+++ problem_30.py
@@ -1,10 +1,10 @@
 # Problem 30: Calculate area of circle
 # Find and fix the error
 
-def area_of_circle(radius):
+def area_of_circle(r):
     pi = 3.14
-    area = pi * radius * radius
+    area = pi*r**2
     return area
 
 r = "5"
-print(f"Area: {area_of_circle(r)}")
+print(f"Area: {area_of_circle(float(r))}")
