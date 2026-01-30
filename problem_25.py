23f2033f4ef292c17f34c20d8394639fd7d27548
nelsonanto043 <nelsonanto043@gmail.com>
2026-01-30 14:25:34 +0530

    Problem 25 solved

--- problem_25.py
+++ problem_25.py
@@ -2,10 +2,7 @@
 # Find and fix the error
 
 def gcd(a, b):
-    while b != 0:
-        temp = b
-        b = a % b
-        a = temp
+    while b:
+        a, b = b, a % b
     return a
-
 print(f"GCD of 48 and 18: {gcd(48, 18)}")
