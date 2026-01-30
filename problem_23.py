de6e7e88fe0f7ce63fc62cf1d1e9147e27b83d2a
nelsonanto043 <nelsonanto043@gmail.com>
2026-01-30 14:17:06 +0530

    Problem 23 solved

--- problem_23.py
+++ problem_23.py
@@ -7,6 +7,7 @@ negative = 0
 for num in numbers:
     if num > 0:
         positive += 1
-    else:
+    elif num < 0:
         negative += 1
-print(f"Positive: {positive}, Negative: {negative}")
+print(f"Positive: {positive}")
+print(f"Negative: {negative}")
